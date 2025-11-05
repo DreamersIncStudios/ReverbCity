05395bf988698d18c66b99df3f5b7033cd33d2a0
dreamer <dreamers.inc12@gmail.com>
2025-11-05 07:12:21 -0500

    "Enhanced `UISceneManager` with pop-up panel support and added customizable text and actions for UI elements."

--- Assets/Scripts/Reverb City Game Code/GameCode/UI/UISceneManager.cs
+++ Assets/Scripts/Reverb City Game Code/GameCode/UI/UISceneManager.cs
@@ -11,6 +11,14 @@ namespace DreamersInc.ReverbCity.UI
     public class UISceneManager : UIManager
     {
         private VisualElement rootElement;
+        [SerializeField]
+        private string headerText;
+        [SerializeField]
+        private string bodyText;
+        [SerializeField]
+        private string buttonText;
+        [SerializeField]
+        private Action buttonAction;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -21,9 +29,10 @@ namespace DreamersInc.ReverbCity.UI
         public override IEnumerator Generate()
         {
             StartCoroutine(base.Generate());
-            rootElement = Create("Background");
-            
-            Root.Add(rootElement);
+            var popUpPanel = Create<PopUpPanel>();
+            popUpPanel.SetText(headerText, bodyText);
+            popUpPanel.SetButton(buttonText, buttonAction);
+            rootElement.Add(popUpPanel);
             yield return null;
         }
 
@@ -38,6 +47,28 @@ namespace DreamersInc.ReverbCity.UI
         public PopUpPanel()
         {
             AddToClassList("PopUpPanel");
+            AddToClassList("Background");
+            var header = Create<Label>("Header");
+            var body = Create<Label>("Body");
+            var startButton = Create<Button>("StartButton");
+            startButton.Focus();
+            Add(header);
+            Add(body);
+            Add(startButton);
+        }
+        public void SetText(string header, string body)
+        {
+            var headerLabel = this.Q<Label>("Header");
+            var bodyLabel = this.Q<Label>("Body");
+            headerLabel.text = header;
+            bodyLabel.text = body;
+        }
+        
+        public void SetButton(string text, Action action)
+        {
+            var button = this.Q<Button>("StartButton");
+            button.text = text;
+            button.clicked += action;
         }
     }
 }
