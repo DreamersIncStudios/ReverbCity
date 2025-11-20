f82392ff2ba73a5f7aca053f4c3030857dab7a02
Dreamer <dreamersinc12@gmail.com>
2025-11-20 17:45:53 -0500

    Add PlayerHealthBar visual element to Player UI
    - Created `PlayerHealthBar` class extending `VisualElement` in the Player UI hierarchy.
    - Added support for player health, mana, and related visual elements (frames and masks).
    - Included necessary metadata files for the new UI structure.

---
+++ Assets/Scripts/Player UI/VisualElements/PlayerHealthBar.cs
@@ -0,0 +1,15 @@
+using UnityEngine.UIElements;
+
+namespace DreamersInc.PlayerUI.VisualElements
+{
+    public class PlayerHealthBar: VisualElement
+    {
+        public Image PlayerImage, HealthBar, ManaBar, Frame;
+        public Image PlayerImageMask, HealthBarMask, ManaBarMask;
+
+        public PlayerHealthBar()
+        {
+            
+        }
+    }
+}
